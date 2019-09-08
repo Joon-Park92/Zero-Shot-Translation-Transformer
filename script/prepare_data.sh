11e91e48d72079d38b2bf0a52cdb11402301222a
Sean Park <sunjun1225@gmail.com>
2019-09-08 22:05:29 +0900

    correct the incorrect for loop syntax

--- script/prepare_data.sh
+++ script/prepare_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # PREPROCESS DATA
-DOWN_DIR=./open_subtitle2018
+DOWN_DIR=/home/slayer/cloned/Zero-Shot-Translation-Transformer/script/open_subtitle2018
 L1=$DOWN_DIR/OpenSubtitles.en-ko.en
 L2=$DOWN_DIR/OpenSubtitles.en-ko.ko
 TEST_SIZE=10000
@@ -12,8 +12,8 @@ VOCABULARY_THRESHOLD=50
 
 # DOWNLOAD PARALLEL LANGUAGE FILE
 download () {
-#  mkdir $DOWN_DIR 
-#  wget http://opus.nlpl.eu/download.php\?f\=OpenSubtitles/v2018/moses/en-ko.txt.zip
+  mkdir $DOWN_DIR 
+  wget http://opus.nlpl.eu/download.php\?f\=OpenSubtitles/v2018/moses/en-ko.txt.zip
   unzip download.php?f=OpenSubtitles%2Fv2018%2Fmoses%2Fen-ko.txt.zip -d $DOWN_DIR
 }
 
@@ -23,25 +23,23 @@ shuffle () {
 }
 
 preprocessing () {	
-  RAW_FILE=$1
 
-  echo CLEAN $RAW_FILE
-  # it works only for ascii encoding
-#  sed -e "s/[^[:print:]]//g" -i $RAW_FILE
-  # for utf-8 
-  sed -e 's/\xc2\x91\|\xc2\x92\|\xc2\xa0\|\xe2\x80\x8e//' -i $RAW_FILE
-  sed -e "s///g" -i $RAW_FILE
-  sed -e "s/\s/ /g" -i $RAW_FILE
-  sed -e "s/[ ]{2,}/ /g" -i $RAW_FILE
+  echo CLEAN $1
 
-  echo SPLIT $RAW_FILE
-  TEST_FILE=$RAW_FILE.test
-  VAL_FILE=$RAW_FILE.val
-  TRAIN_FILE=$RAW_FILE.train
+  # REMOVE INVALID UTF-8 CHARACTERS
+  sed -e 's/[^[:print:]]//g' -i $1
+  sed -e "s///g" -i $1
+  sed -e "s/\s/ /g" -i $1
+  sed -e "s/[ ]{2,}/ /g" -i $1
 
-  sed -n "1,$TEST_SIZE p" $RAW_FILE > $TEST_FILE
-  sed -n "$(($TEST_SIZE+1)),$(($TEST_SIZE+$VAL_SIZE)) p" $RAW_FILE > $VAL_FILE
-  sed -n "$(($TEST_SIZE+$VAL_SIZE+1)),$ p" $RAW_FILE > $TRAIN_FILE
+  echo SPLIT $1
+  TEST_FILE=$1.test
+  VAL_FILE=$1.val
+  TRAIN_FILE=$1.train
+
+  sed -n "1,$TEST_SIZE p" $1 > $TEST_FILE
+  sed -n "$(($TEST_SIZE+1)),$(($TEST_SIZE+$VAL_SIZE)) p" $1 > $VAL_FILE
+  sed -n "$(($TEST_SIZE+$VAL_SIZE+1)),$ p" $1 > $TRAIN_FILE
 
   CODES_FILE=$TRAIN_FILE.codes
   VOCAB_FILE=$TRAIN_FILE.vocab
@@ -50,24 +48,25 @@ preprocessing () {
   subword-nmt learn-bpe -s $NUM_BPE_OPERATIONS < $TRAIN_FILE > $CODES_FILE
   subword-nmt apply-bpe -c $CODES_FILE < $TRAIN_FILE | subword-nmt get-vocab > $VOCAB_FILE
 
-  for FILE in $TRAIN_FILE $VAL_FILE $TEST_FILE
-    do
-      subword-nmt apply-bpe -c $CODES_FILE \
-        --vocabulary $VOCAB_FILE \
-        --vocabulary-threshold $VOCABULARY_THRESHOLD < $FILE > $FILE.ready
-    done
+  subword-nmt apply-bpe -c $CODES_FILE \
+    --vocabulary $VOCAB_FILE \
+    --vocabulary-threshold $VOCABULARY_THRESHOLD < $TRAIN_FILE > $TRAIN_FILE.ready
+
+  subword-nmt apply-bpe -c $CODES_FILE \
+    --vocabulary $VOCAB_FILE \
+    --vocabulary-threshold $VOCABULARY_THRESHOLD < $VAL_FILE > $VAL_FILE.ready
+
+  subword-nmt apply-bpe -c $CODES_FILE \
+    --vocabulary $VOCAB_FILE \
+    --vocabulary-threshold $VOCABULARY_THRESHOLD < $TEST_FILE > $TEST_FILE.ready
 }
 
 main () {
 
   download 
   shuffle
-
-  for LANG in $L1 $L2
-    do
-      preprocessing $LANG
-    done
-
+  preprocessing $L1.shuf
+  preprocessing $L2.shuf
   # ./build_dictionary.py 
 }
 
